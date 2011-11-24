29e4771c4216529b1edba32cc2f4bc467fbdadd0
Pete Goodliffe <pgoodliffe@numark.com>
2011-11-24 14:36:33 +0000

    Enough scaffolding to include the header and create a failing unit test. I broke the build :-)

---
+++ test.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// tests.cpp
+// Copyright (c) 2011 Pete Goodliffe. All rights reserved
+//============================================================================
+
+#include "skip_list.h"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE( "smoketest", "" )
+{
+    REQUIRE(false);
+}
+
