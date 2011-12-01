ae9b0ae53cceca1d06d978c7b4bf69a251842152
Pete Goodliffe <pgoodliffe@numark.com>
2011-12-01 10:47:12 +0000

    Put boost headers into the gitignore

---
+++ benchmarks/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  benchmarks
+//
+//  Created by Pete Goodliffe on 01/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
